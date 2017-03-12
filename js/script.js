3998b169bfb78c574579688f578426ab85e2288d
JO-NOTEBOOK\Jo-NOTEBOOK <alittle.artist@hotmail.com>
2017-03-12 22:17:08 +0700

    Install OwlCarousel

--- js/script.js
+++ js/script.js
@@ -6,9 +6,18 @@ $(document).ready(function () {
     // Function create
     function loadPage() {
         $("#div1").load("Map.html");
+        $("#script").load("script.html");
     }
 
     loadPage();
+    $(".owl-carousel").owlCarousel(
+        {
+            loop: true,
+            margin: 10,
+            nav: true,
+            singleItem: true
+        }
+    );
 });
 
 
