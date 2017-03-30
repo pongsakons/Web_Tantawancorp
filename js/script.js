df3e257e6db7027ed13b14249e180514e54bb64c
JO-NOTEBOOK\Jo-NOTEBOOK <alittle.artist@hotmail.com>
2017-03-31 00:31:24 +0700

    Update code

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
     });
     hamburgerToggle();
     //Call slider
-    $(".owl-carousel").owlCarousel(
+    $(".owl-carousel.-main").owlCarousel(
         {
             loop: true,
             singleItem: true,
@@ -36,14 +36,23 @@ $(document).ready(function () {
                 "<i class='fa fa-angle-right'></i>"
             ],
             autoHeight: true,
-            // autoPlay:true,
+            autoPlay:true,
             slideSpeed: 1000
         },
         $(function () {
-            var owlCarousel = $(".owl-carousel .owl-controls");
+            var owlCarousel = $(".owl-carousel.-main .owl-controls");
             owlCarousel.addClass("col-lg-10 col-lg-offset-1 visible-lg-block");
         })
     );
+    $(".owl-carousel-sub").owlCarousel({
+        loop: true,
+        autoPlay:true,
+        items : 10, //10 items above 1000px browser width
+        itemsDesktop : [1000,5], //5 items between 1000px and 901px
+        itemsDesktopSmall : [1000,5], // betweem 900px and 601px
+        itemsTablet: [1000,5], //2 items between 600 and 0
+        itemsMobile : [1000,5] // itemsMobile disabled - inherit from itemsTablet option
+    });
     // tabs responsive
     // $(function () {
     //     $('.nav-tabs').responsiveTabs();
